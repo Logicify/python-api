b0e93bddcb8ee7e135d67166f217249a1610b76f
Dmitry Berezovsky <dmitry.berezovsky@logicify.com>
2017-07-04 17:16:04 +0300

    Fixed formatting

--- api_commons/dto.py
+++ api_commons/dto.py
@@ -20,6 +20,7 @@
 #    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 #    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 #    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from collections import Iterable
 from django.conf import settings
 from rest_framework.fields import empty, Field
